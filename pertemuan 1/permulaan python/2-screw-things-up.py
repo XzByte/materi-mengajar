0e025bb9b27bf5fbeaafa7196e3eebf38d6455bc
SuprAdmin <91585579+XzByte@users.noreply.github.com>
2024-08-03 16:03:39 +0700

    Add files via upload

---
+++ pertemuan 1/permulaan python/2-screw-things-up.py
@@ -0,0 +1,24 @@
+# mencari solusi dalam pengaplikasian string pada python
+
+
+# Exercise 1
+# line dibawah pasti error dan butuh solving untuk 
+print("hi)
+
+# We didn't close the double quotes at the end of the string.
+# The line above needed to have been:
+# print("hi")
+
+
+# Exercise 2
+''' untuk codingan dibawah, walau yang atas dah bener, bakal error karena (runtime error), jadi harus solve 2 2nya,
+    karena variable hello belum ada '''
+print(hello)
+
+# excercise 2 tujuannya buat ngeprint string hello dari variable
+# tapi variable nya kan belum ada, jadi bakal error
+#
+# solve nya begini:
+#
+# my_string = "hello"
+# print(my_string)
---
+++ pertemuan 1/permulaan python/3-create-a-variable.py
@@ -0,0 +1,14 @@
+# 3.3 - Create a Variable
+# Solutions to review exercies
+
+
+# yang ini buat kunci jawaban
+
+
+# membuat string lalu di print
+print("hello")
+
+
+# buat string, lalu masukan di variable dan ditampilkan
+my_string = "hi"
+print(my_string)
